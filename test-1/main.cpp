cff02139eb08db1f10bd0da8e0a3c4dd9a8472d0
zjm <1219032974@qq.com>
2021-10-10 10:29:10 +0800

    first test

---
+++ test-1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+using namespace std;
+#include <list>
+#include <vector>
+#include <windows.h>
+//#include <iostream>
+#include <memory>
+// cout << c << endl;
+#include <setjmp.h>
+#include <stdarg.h>
+
+int main()
+{
+    int num=61;
+   
+    printf( " %d", num );
+}
